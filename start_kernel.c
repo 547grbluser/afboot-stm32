bf003d52e2e80175cdb3e5aa36e657e7afeba927
hhuysqt <hyq9606@126.com>
2018-01-21 22:37:15 +0800

    DTB is just following the loader

--- start_kernel.c
+++ start_kernel.c
@@ -1,9 +1,13 @@
 #include <stdlib.h>
 #include <stdint.h>
 
+extern unsigned int _end_text;
+extern unsigned int _start_data;
+extern unsigned int _end_data;
 void start_kernel(void)
 {
 	void (*kernel)(uint32_t reserved, uint32_t mach, uint32_t dt) = (void (*)(uint32_t, uint32_t, uint32_t))(KERNEL_ADDR | 1);
 
-	kernel(0, ~0UL, DTB_ADDR);
+	/* the DTB is just after the bootloader */
+	kernel(0, ~0UL, /*DTB_ADDR*/ (uint32_t)&_end_text + (uint32_t)&_end_data - (uint32_t)&_start_data);
 }
