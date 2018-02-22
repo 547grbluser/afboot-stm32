e56a000916c40753c4efbde86842e525f7679f4d
hhuysqt <1020988872@qq.com>
2018-02-22 16:22:25 +0800

    init LTDC on startup

--- stm32429i-ucpc.c
+++ stm32429i-ucpc.c
@@ -77,7 +77,99 @@ static void clock_setup(void)
 	*RCC_AHB1LPENR &= ~RCC_AHB1LPENR_OTGHSULPILPEN;
 }
 
+static void set_display(uint32_t FBAddr)
+{
+	volatile uint32_t *RCC_CR = (void *)(RCC_BASE + 0x00);
+	volatile uint32_t *RCC_PLLSAICFGR = (void*)(RCC_BASE + 0x88);
 
+	gpio_set_alt(gpio_base, 'A', 3,  0, GPIOx_OSPEEDR_OSPEEDRy_FAST, 0, 14);
+	gpio_set_alt(gpio_base, 'B', 0,  0, GPIOx_OSPEEDR_OSPEEDRy_FAST, 0, 9);
+	gpio_set_alt(gpio_base, 'B', 1,  0, GPIOx_OSPEEDR_OSPEEDRy_FAST, 0, 9);
+	gpio_set_alt(gpio_base, 'B', 8,  0, GPIOx_OSPEEDR_OSPEEDRy_FAST, 0, 14);
+	gpio_set_alt(gpio_base, 'B', 9,  0, GPIOx_OSPEEDR_OSPEEDRy_FAST, 0, 14);
+	gpio_set_alt(gpio_base, 'B', 10, 0, GPIOx_OSPEEDR_OSPEEDRy_FAST, 0, 14);
+	gpio_set_alt(gpio_base, 'G', 6,  0, GPIOx_OSPEEDR_OSPEEDRy_FAST, 0, 14);
+	gpio_set_alt(gpio_base, 'G', 11, 0, GPIOx_OSPEEDR_OSPEEDRy_FAST, 0, 14);
+	gpio_set_alt(gpio_base, 'G', 12, 0, GPIOx_OSPEEDR_OSPEEDRy_FAST, 0, 9);
+	gpio_set_alt(gpio_base, 'H', 10, 0, GPIOx_OSPEEDR_OSPEEDRy_FAST, 0, 14);
+	gpio_set_alt(gpio_base, 'H', 11, 0, GPIOx_OSPEEDR_OSPEEDRy_FAST, 0, 14);
+	gpio_set_alt(gpio_base, 'H', 13, 0, GPIOx_OSPEEDR_OSPEEDRy_FAST, 0, 14);
+	gpio_set_alt(gpio_base, 'H', 14, 0, GPIOx_OSPEEDR_OSPEEDRy_FAST, 0, 14);
+	gpio_set_alt(gpio_base, 'I', 0,  0, GPIOx_OSPEEDR_OSPEEDRy_FAST, 0, 14);
+	gpio_set_alt(gpio_base, 'I', 1,  0, GPIOx_OSPEEDR_OSPEEDRy_FAST, 0, 14);
+	gpio_set_alt(gpio_base, 'I', 2,  0, GPIOx_OSPEEDR_OSPEEDRy_FAST, 0, 14);
+	gpio_set_alt(gpio_base, 'I', 9,  0, GPIOx_OSPEEDR_OSPEEDRy_FAST, 0, 14);
+	gpio_set_alt(gpio_base, 'I', 10, 0, GPIOx_OSPEEDR_OSPEEDRy_FAST, 0, 14);
+
+	// set ltdc clock
+	*RCC_PLLSAICFGR = (204 << 6) | (7 << 24) | (4 << 28);
+	*RCC_CR |= (1L << 28);
+	while ((*RCC_CR & (1L << 29)) == 0);
+
+	// configure LTDC to 640x480 VGA timings
+	struct stm32_ltdc_regs {
+		uint32_t reserved0[2];  /* Reserved, 0x00-0x04 */
+		uint32_t sscr;          /* Synchronization Size Configuration Register */
+		uint32_t bpcr;          /* Back Porch Configuration Register,          */
+		uint32_t awcr;          /* Active Width Configuration Register,        */
+		uint32_t twcr;          /* Total Width Configuration Register,         */
+		uint32_t gcr;           /* Global Control Register,                    */
+		uint32_t reserved1[2];  /* Reserved, 0x1C-0x20 */
+		uint32_t srcr;          /* Shadow Reload Configuration Register,       */
+		uint32_t reserved2[1];  /* Reserved, 0x28 */
+		uint32_t bccr;          /* Background Color Configuration Register,    */
+		uint32_t reserved3[1];  /* Reserved, 0x30 */
+		uint32_t ier;           /* Interrupt Enable Register,                  */
+		uint32_t isr;           /* Interrupt Status Register,                  */
+		uint32_t icr;           /* Interrupt Clear Register,                   */
+		uint32_t lipcr;         /* Line Interrupt Position Configuration Regis */
+		uint32_t cpsr;          /* Current Position Status Register            */
+		uint32_t cdsr;          /* Current Display Status Register             */
+	} *STM32_LTDC = (void*)0x40016800;
+
+	struct stm32_ltdc_layer_regs {
+		uint32_t cr;            /* Control                                   */
+		uint32_t whpcr;         /* Window Horizontal Position Configuration  */
+		uint32_t wvpcr;         /* Window Vertical Position Configuration    */
+		uint32_t ckcr;          /* Color Keying Configuration                */
+		uint32_t pfcr;          /* Pixel Format Configuration                */
+		uint32_t cacr;          /* Constant Alpha Configuration              */
+		uint32_t dccr;          /* Default Color Configuration               */
+		uint32_t bfcr;          /* Blending Factors Configuration            */
+		uint32_t reserved0[2];  /*!< Reserved */
+		uint32_t cfbar;         /* Color Frame Buffer Address                */
+		uint32_t cfblr;         /* Color Frame Buffer Length                 */
+		uint32_t cfblnr;        /* ColorFrame Buffer Line Number             */
+		uint32_t reserved1[3];  /*!< Reserved */
+		uint32_t clutwr;        /* CLUT Write                                */
+	} *STM32_LTDC_LAYER1 = (void*)0x40016884;
+
+	// VGA timings
+	STM32_LTDC->sscr = ((96) << 16)           | (2);
+	STM32_LTDC->bpcr = ((96+48) << 16)        | (2+33);
+	STM32_LTDC->awcr = ((96+48+640) << 16)    | (2+33+480);	// 640 x 480
+	STM32_LTDC->twcr = ((96+48+640+16) << 16) | (2+33+480+10);
+	STM32_LTDC->bccr = 0x00000000; // black background
+
+	// Layer configuration
+	STM32_LTDC_LAYER1->whpcr  = ((95+48+641) << 16) | (95+48+2);
+	STM32_LTDC_LAYER1->wvpcr  = ((1+33+480) << 16) | (1+33);
+	STM32_LTDC_LAYER1->pfcr   = 2;	// RGB565
+	STM32_LTDC_LAYER1->dccr   = 0x00000000;
+	STM32_LTDC_LAYER1->cacr   = 255;	// alpha
+	STM32_LTDC_LAYER1->bfcr   = (1024 | 5);
+	STM32_LTDC_LAYER1->cfbar  = FBAddr;	// Frame buffer address
+	STM32_LTDC_LAYER1->cfblr  = (640*2)<<16 | (640*2 + 3);
+	STM32_LTDC_LAYER1->cfblnr  = 480;
+	
+	STM32_LTDC->gcr &= (uint32_t)0x0FFE888F;
+	STM32_LTDC->gcr |= (uint32_t)0x00010000;
+	STM32_LTDC_LAYER1->cr |= (uint32_t)0x00000001;
+	STM32_LTDC->srcr = (uint32_t)0x00000001;
+
+	// Enable The LCD
+	STM32_LTDC->gcr |= (uint32_t)0x00000001;
+}
 
 static void fmc_wait_busy(void)
 {
@@ -95,8 +187,12 @@ int main(void)
 	volatile uint32_t *FMC_SDTR1 = (void *)(FMC_BASE + 0x148);
 	volatile uint32_t *FMC_SDCMR = (void *)(FMC_BASE + 0x150);
 	volatile uint32_t *FMC_SDRTR = (void *)(FMC_BASE + 0x154);
-	//volatile uint32_t *SYSCFG_MEMRMP = (void *)(SYSCFG_BASE + 0x00);
+	volatile uint32_t *SYSCFG_MEMRMP = (void *)(SYSCFG_BASE + 0x00);
 	int i;
+	
+	// disable write buffer on cortex m4
+	volatile uint32_t *NVIC_ACTLR = (void*)0xe000e008;
+	*NVIC_ACTLR = 0x00000002;
 
 	if (*FLASH_CR & FLASH_CR_LOCK) {
 		*FLASH_KEYR = 0x45670123;
@@ -115,7 +211,7 @@ int main(void)
 	gpio_set_fmc(gpio_base, 'D', 1); //D3
 	gpio_set_fmc(gpio_base, 'D', 4); //D13
 	gpio_set_fmc(gpio_base, 'D', 5); //D14
-	gpio_set_fmc(gpio_base, 'D', 6); //D15
+	gpio_set_input_pullup(gpio_base, 'D', 6); //D15
 	gpio_set_fmc(gpio_base, 'D', 7); //A16
 	gpio_set_fmc(gpio_base, 'D', 8); //A17
 	gpio_set_fmc(gpio_base, 'D', 9); //A18
@@ -170,7 +266,25 @@ int main(void)
 	fmc_wait_busy();
 
 	*FMC_SDRTR |= (636*8)<<1; // refresh rate
+	fmc_wait_busy();
 	//*FMC_SDCR1 &= 0xFFFFFDFF;
+
+	// setup nand flash
+	volatile uint32_t *FMC_NAND_PCR2  = (void *)(FMC_BASE + 0x60);
+	volatile uint32_t *FMC_NAND_PMEM2 = (void *)(FMC_BASE + 0x68);
+	volatile uint32_t *FMC_NAND_PATT2 = (void *)(FMC_BASE + 0x6c);
+	// ECC 2k, TAR=TCLR=1, ECC disable, 8 bits, NAND flash, wait feature
+	*FMC_NAND_PCR2 = 0x00060008;
+	// SET = 2, WAIT = 4, HOLD = 2, HIZ = 2
+	*FMC_NAND_PMEM2 =
+	*FMC_NAND_PATT2 = 0x02020402;
+	// enable NAND
+	*FMC_NAND_PCR2 |= 0x00000004;
+	
+	// SDRAM Bank 1 mapped to 0x80000000 and 0x0
+	*SYSCFG_MEMRMP = 0x00000404;
+
+	set_display(0x83f00000);
 
 	gpio_set_usart(gpio_base, 'A', 9, 7);
 	gpio_set_usart(gpio_base, 'A', 10, 7);
@@ -178,7 +292,6 @@ int main(void)
 	usart_setup(usart_base, 84000000);
 	usart_putch(usart_base, '.');
 
-	//*SYSCFG_MEMRMP = 0x4;
 
 	start_kernel();
 
